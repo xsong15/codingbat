3dba200ad0a433894c3ae46f440260e9b1d144e3
xsong15 <48596163+xsong15@users.noreply.github.com>
2020-02-11 14:57:21 -0500

    update

---
+++ list-1/same_first_last.py
@@ -0,0 +1,14 @@
+def same_first_last(nums):
+    """
+    Given an array of ints, return True if the array is 
+    length 1 or more, and the first element and the last 
+    element are equal.
+    """
+    return len(nums) >= 1 and nums[-1] == nums[0]
+
+print(same_first_last([1, 2, 3])) 
+#False
+print(same_first_last([1, 2, 3, 1]))
+# True
+print(same_first_last([1, 2, 1]))
+# True
