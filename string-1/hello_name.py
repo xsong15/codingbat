203715532b33dc87bf2747b2a75a80b39bb8d31f
xsong15 <48596163+xsong15@users.noreply.github.com>
2020-02-12 23:34:06 -0500

    update

--- string-1/hello_name.py
+++ string-1/hello_name.py
@@ -1,9 +1,9 @@
 def hello_name(name):
     """
-    Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+    Given a string name, e.g. "Bob", return a greeting of 
+    the form "Hello Bob!".
     """
     return 'Hello ' + name + '!'
-
 
 print(hello_name('Bob'))
 print(hello_name('Alice'))
