a204fa8026216c95188fed68ce5deff75d223c39
xsong15 <48596163+xsong15@users.noreply.github.com>
2020-02-20 12:17:31 -0500

    update

--- string-1/make_abba.py
+++ string-1/make_abba.py
@@ -4,3 +4,8 @@ def make_abba(a, b):
     putting them together in the order abba, e.g. "Hi" 
     and "Bye" returns "HiByeByeHi".
     """
+    return a + b * 2 + a
+
+print(make_abba("Hi", "Bye"))
+
+# print(str(a, b, b, a))
---
+++ string-1/make_tags.py
@@ -0,0 +1,13 @@
+def make_tags(tag, word):
+    """
+    The web is built with HTML strings like "<i>Yay</i>" 
+    which draws Yay as italic text. In this example, the 
+    "i" tag makes <i> and </i> which surround the word "Yay". 
+    Given tag and word strings, create the HTML string with 
+    tags around the word, e.g. "<i>Yay</i>".
+    """
+    return "<"+tag+">"+word+"<"+tag+">"
+
+print(make_tags('i', 'Yay')) #→ '<i>Yay</i>'
+print(make_tags('i', 'Hello')) #→ '<i>Hello</i>'
+print(make_tags('cite', 'Yay')) #→ '<cite>Yay</cite>'
